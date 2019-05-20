4ff1be34a67dbd6de385d0a13429c45a04372934
Roman Volobuiev <roman.volobuev@es.bet>
2019-05-20 10:10:43 +0300

    add pwa and amp

---
+++ src/sw.js
@@ -0,0 +1,45 @@
+const cacheName = "cache-v1";
+const precacheResources = [
+  "/",
+  "index.html",
+  "styles/main.css",
+  "js/",
+  "images/"
+];
+
+self.addEventListener("install", event => {
+  console.log("Service worker installing...");
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(precacheResources);
+    })
+  );
+  // Add a call to skipWaiting here
+});
+
+self.addEventListener("activate", event => {
+  console.log("Service worker activating...");
+  console.log("Activating new service worker...");
+
+  const cacheWhitelist = [precacheResources];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
